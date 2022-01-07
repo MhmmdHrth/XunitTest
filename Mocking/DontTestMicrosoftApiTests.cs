6b64f0902517724ab0215c2136016188693f0155
mhmmdhrth <harith781@gmail.com>
2022-01-08 00:31:27 +0800

    MOQ

---
+++ Mocking/DontTestMicrosoftApiTests.cs
@@ -0,0 +1,25 @@
+﻿using Mocking.Units;
+using Moq;
+using System.IO;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Mocking
+{
+    public class DontTestMicrosoftApiTests
+    {
+        public readonly Mock<IFiles> _fileMock = new();
+
+        [Fact]
+        public async Task WritesToFileStream()
+        {
+            var service = new DontTestMicrosoftApi(_fileMock.Object);
+            var memoryStream = new MemoryStream();
+            _fileMock.Setup(x => x.OpenWriteStreamTo("path")).Returns(memoryStream);
+
+            await service.SaveFile("path", new MemoryStream(new byte[] { 2, 3, 4, 5 }));
+
+            Assert.Equal(4, memoryStream.Length);
+        }
+    }
+}
