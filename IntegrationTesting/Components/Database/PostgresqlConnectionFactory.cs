51a9914543bf2de952791638addcea18cf2d788b
mhmmdhrth <harith781@gmail.com>
2022-01-08 00:41:58 +0800

    Integration Testing

---
+++ IntegrationTesting/Components/Database/PostgresqlConnectionFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace IntegrationTesting.Components.Database
+{
+    public class PostgresqlConnectionFactory : IAsyncDisposable
+    {
+        private readonly string _connectionString;
+
+        public PostgresqlConnectionFactory(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        private NpgsqlConnection _connection;
+
+        public async Task<NpgsqlConnection> Create()
+        {
+            // not async safe
+            if (_connection != null)
+                return _connection;
+
+            _connection = new(_connectionString);
+            await _connection.OpenAsync();
+            return _connection;
+        }
+
+        public ValueTask DisposeAsync() => _connection.DisposeAsync();
+    }
+}
