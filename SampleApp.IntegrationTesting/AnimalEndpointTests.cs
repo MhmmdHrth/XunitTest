f7f1eb487da54e4934aaec21ca42b301d74bab08
mhmmdhrth <harith781@gmail.com>
2022-01-12 01:31:49 +0800

    test application using webApplicationFactory from MVC Testing package

---
+++ SampleApp.IntegrationTesting/AnimalEndpointTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using SampleApp.Models;
+using System.Net;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SampleApp.IntegrationTesting
+{
+    public class AnimalEndpointTests : IClassFixture<WebApplicationFactory<Startup>> //WebApplicationFactory will mock instance of application
+    {
+        private readonly WebApplicationFactory<Startup> _applicationFactory;
+        public AnimalEndpointTests(WebApplicationFactory<Startup> applicationFactory)
+        {
+            _applicationFactory = applicationFactory.WithWebHostBuilder(x =>
+            {
+                x.addCustomeService();
+            });
+        }
+
+        [Fact]
+        public async Task Get_SingleAnimal()
+        {
+            var client = _applicationFactory.CreateClient();
+
+            var response = await client.GetAsync("/api/animal");
+            var content = await response.Content.ReadAsStringAsync(); //to debug if got error
+            var animal = await response.Content.ReadFromJsonAsync<Animal>();
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal(2, animal.Id);
+            Assert.NotNull(animal);
+        }
+    }
+}
---
+++ SampleApp.IntegrationTesting/Mocks/AnimalServiceMock.cs
@@ -0,0 +1,18 @@
+﻿using SampleApp.Models;
+using SampleApp.Services;
+
+namespace SampleApp.IntegrationTesting.Mocks
+{
+    public class AnimalServiceMock : IAnimalService
+    {
+        public Animal GetAnimal()
+        {
+            return new()
+            {
+                Id = 2,
+                Name = "Foo2",
+                Type = "Bar2",
+            };
+        }
+    }
+}
